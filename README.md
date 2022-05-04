8b62af58236c01b9aadca9c37bb72d7229026c0b
eniallator <eniallatormc@gmail.com>
2022-05-04 17:51:47 +0100

    Initial commit, added everything

---
+++ README.md
@@ -0,0 +1,145 @@
+# Invoice Generator
+
+Welcome to my invoice generator! It contains a custom templating language for inserting context variables/directives.
+
+## How It Works
+
+If you like the look of the [example invoice pdf](./example-invoice.pdf) or the [example invoice LaTeX](./example-invoice.tex), the way that it is generated is using the [invoice template](./invoice_template.tex) which refers to the [invoice item](./invoice_item.tex) LaTeX file for generating individual invoice items.
+
+These templates are then filled out with context variables starting from the [example invoice config](./raw_invoices/johnDoe.2000.01.01.cfg). This then also imports the [example client](./clients/johnDoe.cfg) and the [example my_info config](./my_info.cfg).
+
+## Config Files
+
+Config files use the properties syntax as described in the [configparser documentation](https://docs.python.org/3/library/configparser.html#supported-ini-file-structure).
+
+### Importing Other Config Files
+
+Imports can be specified in the `META` section of config with the `import` option. All imports must be separated with newlines. You also don't specify the extension when importing, the `.cfg` extension is added implicitly. For example:
+
+```properties
+[META]
+
+import=./default
+  ./anothercfg
+  ../clients/my-client
+```
+
+### Invoice Items
+
+Invoice items are sections which have the `ITEM_` prefix, where what follows is the item ID. e.g `ITEM_1` has an ID of `1`, but `ITEM_ONE` has an ID of `ONE`.
+
+Each item must have the following options: `rate`, and `hrs`. These are then used to work out a `subtotal` option. These subtotals are also added up into a `total_due` option in the `META` section. For example:
+
+```properties
+[ITEM_1]
+
+hrs=5
+rate=6
+
+[ITEM_2]
+
+hrs=2
+rate=3
+```
+
+when parsed will become the equivalent of passing the following in:
+
+```properties
+[META]
+
+total_due=36.00
+
+[ITEM_1]
+
+id=1
+subtotal=30.00
+hrs=5.00
+rate=6.00
+
+[ITEM_2]
+
+id=2
+subtotal=6.00
+hrs=2.00
+rate=3.00
+```
+
+## Template Files
+
+Template files can have context variables inserted (from the config files).
+
+### Context Variables
+
+These have the following format: `{{ section.option }}` and can be inserted anywhere inside the file. The other format you can use is `{{ option }}`, where the section is implicitly set to `META`. For example:
+
+```LaTeX
+\underline{INV DATE} {{ invoice_date }}
+Pay to: {{ PAYMENT.account_name }}
+```
+
+with the following config file input:
+
+```properties
+[META]
+
+invoice_date=2000/01/01
+
+[PAYMENT]
+
+account_name=John Doe
+```
+
+will generate the following LaTeX:
+
+```LaTeX
+\underline{INV DATE} 2000/01/01
+Pay to: John Doe
+```
+
+Note:\
+If the parser cannot find a specific context variable, it will leave that context variable unchanged and not replace it with anything.
+
+### Items Directive
+
+Directives have the following syntax: `{* name args *}`, where `name` is the name of the directive and `args` are the directive's arguments.
+
+The items directive will take in a path of an external LaTeX file, where the file will be parsed with the context variables, except with the addition of the `ITEM` section added to the context variables, which is the current invoice item. For example:
+
+config:
+
+```properties
+[ITEM_1]
+
+hrs=5
+rate=6
+
+[ITEM_2]
+
+hrs=2
+rate=3
+```
+
+invoice template:
+
+```LaTeX
+Invoice Items:
+ID, hrs, rate, subtotal
+{* items ./invoice_item.tex *}
+Total: {{ total_due }}
+```
+
+./invoice_item.tex
+
+```LaTeX
+{{ ITEM.id }}, {{ ITEM.hrs }}, {{ ITEM.rate }}, {{ ITEM.subtotal }}
+```
+
+Will all generate:
+
+```LaTeX
+Invoice Items:
+ID, hrs, rate, subtotal
+1, 5.00, 6.00, 30.00
+2, 2.00, 3.00, 6.00
+Total: 36.00
+```
---
+++ generate.py
@@ -0,0 +1,185 @@
+import re
+import argparse
+from os import path, makedirs
+from subprocess import Popen
+from configparser import ConfigParser
+
+
+class ContextVariables:
+    IMPORT_SEPARATOR = "\n"
+
+    def __init__(self, base_path):
+        self.base_path = base_path
+
+        self.config = ConfigParser()
+
+        self.fetch_imports()
+        self.build_invoice_items()
+
+    def fetch_imports(self):
+        import_queue = [path.realpath(self.base_path)]
+        seen_imports = set(import_queue)
+        errored_imports = set()
+        while len(import_queue) > 0:
+            import_path = import_queue.pop(0)
+            new_config = ConfigParser()
+            success = new_config.read(import_path)
+            if not success:
+                errored_imports.add(import_path)
+                continue
+            for section in new_config.sections():
+                if section not in self.config:
+                    self.config.add_section(section)
+                for option in new_config.options(section):
+                    self.config.set(section, option, new_config.get(section, option))
+            new_imports = set(
+                path.realpath(path.join(path.dirname(import_path), new_import + ".cfg"))
+                if new_import.startswith(".")
+                else path.abspath(new_import + ".cfg")
+                for new_import in self.config.get("META", "import", fallback="").split(
+                    self.IMPORT_SEPARATOR
+                )
+                if new_import
+            )
+            self.config.set("META", "import", "")
+            for new_import in new_imports:
+                if new_import not in seen_imports:
+                    seen_imports.add(new_import)
+                    import_queue.append(new_import)
+        if errored_imports:
+            raise ImportError(
+                "Could not import the following config files:\n"
+                + "\n".join(errored_imports)
+            )
+
+    def build_invoice_items(self):
+        item_prefix = "ITEM_"
+        item_sections = list(
+            sorted(
+                filter(
+                    lambda section: section.startswith(item_prefix),
+                    self.config.sections(),
+                )
+            )
+        )
+        self.invoice_items = [dict(self.config[section]) for section in item_sections]
+        total = 0
+        for item, section in zip(self.invoice_items, item_sections):
+            if "id" not in item:
+                item["id"] = section[len(item_prefix) :]
+            if "." in item["hrs"]:
+                item["hrs"] = f"{float(item['hrs']):.2f}"
+            item["rate"] = f"{float(item['rate']):.2f}"
+            if "subtotal" not in item:
+                item["subtotal"] = f"{float(item['hrs']) * float(item['rate']):.2f}"
+            total += float(item["subtotal"])
+        if "total_due" not in self.config["META"]:
+            self.config.set("META", "total_due", f"{total:.2f}")
+
+    def get_variable(self, var_id, extra={}):
+        var_path = tuple(var_id.split("."))
+        if len(var_path) > 1:
+            section, key = var_path
+        else:
+            section, key = "META", var_path[0]
+        value = self.config.get(section, key, fallback=None)
+        if value is None:
+            return extra.get(section, {}).get(key)
+        return value
+
+
+class DynamicContentParser:
+    context_variable_re = r"{{\s*(?P<var_id>[\w\.]+)\s*}}"
+    directive_re = r"{\*(?P<directive>[^\*]+)\*}"
+
+    def __init__(self, context_variables):
+        self.context_variables = context_variables
+
+    def insert_variable(self, match, extra={}):
+        variable = self.context_variables.get_variable(
+            match.group("var_id"), extra=extra
+        )
+        return variable if variable is not None else match.group(0)
+
+    def directive_items(self, args):
+        item_template = args.strip()
+        items_string = ""
+        if not path.exists(item_template):
+            return None
+        with open(item_template, "r") as file_handle:
+            for item in self.context_variables.invoice_items:
+                items_string += re.sub(
+                    self.context_variable_re,
+                    lambda match, item=item: self.insert_variable(
+                        match, extra={"ITEM": item}
+                    ),
+                    file_handle.read(),
+                )
+        return items_string
+
+    def insert_directive(self, match):
+        name = match.group("directive").split()[0]
+        args = match.group("directive").strip()[len(name) :].strip()
+        directive_name = f"directive_{name}"
+        if hasattr(self, directive_name):
+            output = getattr(self, directive_name)(args)
+            if output is not None:
+                return output
+        return match.group(0)
+
+    def parse_string(self, contents):
+        return re.sub(
+            self.context_variable_re,
+            self.insert_variable,
+            re.sub(self.directive_re, self.insert_directive, contents),
+        )
+
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser(description="Generate an invoice")
+    arg_parser.add_argument(
+        "--cfg",
+        "-c",
+        help="Config file to fill the invoice out with",
+        required=True,
+        dest="cfg",
+    )
+    arg_parser.add_argument(
+        "--template",
+        "-t",
+        help="LaTeX template file to use with the config variables",
+        default="./invoice_template.tex",
+        dest="template",
+    )
+    arg_parser.add_argument(
+        "--out-path",
+        "-o",
+        help="Output .tex path where to generate the auxiliary/tex/pdf files",
+        default="./out/invoice.tex",
+        dest="out_path",
+    )
+    prog_args = arg_parser.parse_args()
+
+    context_variables = ContextVariables(prog_args.cfg)
+    parser = DynamicContentParser(context_variables)
+    out_dir = path.dirname(prog_args.out_path)
+    makedirs(out_dir, exist_ok=True)
+
+    with open(prog_args.template, "r") as file_handle:
+        generated_invoice = parser.parse_string(file_handle.read())
+
+    with open(prog_args.out_path, "w") as file_handle:
+        file_handle.write(generated_invoice)
+
+    Popen(
+        [
+            "latexmk",
+            "-synctex=1",
+            "-interaction=nonstopmode",
+            "-file-line-error",
+            "-pdf",
+            f"-aux-directory={out_dir}",
+            f"-output-directory={out_dir}",
+            prog_args.out_path,
+        ],
+    ).wait()
